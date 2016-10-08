ddea770228c1048919d5c27acea6d3ccdda91a97
__alexander__ <alexander.ayasca.esquives@gmail.com>
2016-10-08 18:27:04 -0500

    Galería de imágenes de un website (archivos "media" configurados)

--- uxlabs/uxlabs/urls.py
+++ uxlabs/uxlabs/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 url(r'^admin/', admin.site.urls),
     url(r'^', include('apps.contacts.urls', namespace='contacts')),
     url(r'^', include('apps.websites.urls', namespace='websites')),
     url(r'^', include('apps.info.urls', namespace='info')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
